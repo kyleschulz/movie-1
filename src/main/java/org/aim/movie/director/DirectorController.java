471f3c751628aec73fd0a5cdfbb340f6fd02c674
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2020-07-21 18:24:55 -0500

    update

---
+++ src/main/java/org/aim/movie/director/DirectorController.java
@@ -0,0 +1,58 @@
+package org.aim.movie.director;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/api/directors")
+public class DirectorController {
+    @AutoWire
+    private DirectorRepository directorRepository;
+
+    @GetMapping(path = "")
+    public Iterable<Director> getAllDirecors() {
+        return directorRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Director getDirector(@PathVariable(value = "id") Integer id) {
+        Optional<Director> director = directorRepository.findById(id);
+        return director.get();
+    }
+
+    @PostMapping(path = "/")
+    public @ResponseBody String createDirector(@RequestBody Director director) {
+        directorRepository.save(director);
+        return "Saved";
+    }
+
+    @PutMapping(path = "/{id}")
+    public @ResponseBody String updateDirector(@PathVariable(value = "id") Integer id,
+            @RequestBody Director directorDetails) {
+        Optional<Director> optialDirector = directorRepository.findById(id);
+        Director director = optialDirector.get();
+
+        director.setFirstName(directorDetails.getFirstName());
+        director.setLastName(directorDetails.getLastName());
+        director.setDateOfBirth(directorDetails.getDateOfBrith());
+
+        directorRepository.save(director);
+        return "Updated";
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteActor(@PathVariable(value = "id") Integer id) {
+        directorRepository.deleteById(id);
+        return "Deleted";
+    }
+
+}
