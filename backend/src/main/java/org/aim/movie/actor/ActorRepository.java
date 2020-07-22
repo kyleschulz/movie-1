c278d3e250036e324b13607f1382d0550be66d83
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2020-07-22 13:25:49 -0500

    closer update

--- backend/src/main/java/org/aim/movie/actor/ActorRepository.java
+++ backend/src/main/java/org/aim/movie/actor/ActorRepository.java
@@ -1,6 +1,7 @@
 package org.aim.movie.actor;
 
 import org.springframework.data.repository.CrudRepository;
+
 import org.aim.movie.actor.Actor;
 
 public interface ActorRepository extends CrudRepository<Actor, Integer> {
