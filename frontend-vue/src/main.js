962bee5ebbb4c8dca6af094948f9aa52c5da7332
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2020-07-30 20:49:04 -0500

    update commit

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
+require("./assets/main.css");
+
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
---
+++ frontend-vue/src/views/Actors.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="main">
+    <div class="header">
+      <h1 class="title">Actors</h1>
+    </div>
+
+    <div class="content">
+      <table id="actors">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Date of Birth</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="actor in actors" :key="actor.id">
+            <td>{{actor.id}}</td>
+            <td>{{actor.firstName}}</td>
+            <td>{{actor.dateOfBirth}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    {{actors}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Actors",
+  data: () => ({
+    actors: [],
+  }),
+  async mounted() {
+    console.log("actors mounted being");
+    const { data } = await this.$http.get("http://localhost:8080/api/actors");
+    console.log("actors mounted data", data);
+    this.actors = data;
+  },
+};
+</script>
